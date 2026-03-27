1c82cb4948d2f7ffe0b2e89fbbd4f974b8631aa4
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2026-03-27 18:33:18 +0800

    sync https://www.lejuhub.com/robotembodieddata/kuavo_data_challenge/commit/e46a19cfd1c5d4fe1f0f02a406ded11ea8de6a92

--- kuavo_train/train_policy.py
+++ kuavo_train/train_policy.py
@@ -1,4 +1,4 @@
-# Copyright 2025 LejuRobotics
+# Copyright (C) 2025-2026 LejuRobotics.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
